a66c4ea6b41b09b1593e0ed5690662d357a13e1c
Luis Eduardo Alencar <83970615+eduardoadf21@users.noreply.github.com>
2023-07-30 22:51:18 -0300

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-ghdl -a --std=08 -fsynopsys risc16_package.vhd controller.vhd alu.vhd fake_memory.vhd nbitsmux.vhd registers.vhd datapath.vhd testbench_datapath.vhd
-
-ghdl -e --std=08 -fsynopsys testbench_datapath
-
-ghdl -r --std=08 -fsynopsys testbench_datapath --vcd=tb.vcd
